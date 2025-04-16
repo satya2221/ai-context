6dde640b78f68570f7ddcc0c925b5a02696a7392
satya2221 <satyaghifari12@gmail.com>
2025-04-17 06:46:02 +0700

    generate event confirmation

--- event.py
+++ event.py
@@ -1,6 +1,7 @@
 from pm import PromptManager
 from pydantic import BaseModel, Field
-from datetime import date
+from datetime import datetime
+import json
 
 class AnalyzeEvent(BaseModel):
     is_event: bool = Field(description="Information of the query if contain an event") # memberikan informasi yang lebih tepat mengenai skema kita
@@ -25,20 +26,38 @@ def analyze_event(query):
     return is_event, description, confidence_score
 
 def extract_event(query):
-    today = date.today()
+    today = datetime.today()
+    formatted_date = today.strftime("%Y-%m-%d")
     pm = PromptManager()
-    pm.add_message("system", f"Extract event details based on user query, as additional information today date is {today}")
+    pm.add_message("system", f"Extract event details based on user query, as additional information today date is {formatted_date}")
     pm.add_message("user", query)
 
     result = pm.generate_structure(EventDetail)
-    print(result)
+    return result
+
+def generate_confirmation(query):
+    pm = PromptManager()
+    pm.add_message("system", 
+                   """
+                    Create a confirmation message to the user, and ask if it's confirmed.
+
+                    EXAMPLE RESPONSE OUTPUT:
+                    Hey, I will make an event for you with this detail: follow with the event details.
+                    Let me know if it's good!
+                   """)
+    pm.add_message("user", query)
+    
+    return pm.generate()
 
 def run():
     input_query = input("Query: ")
     is_event, description, confidence_score = analyze_event(input_query)
     
     if is_event and confidence_score > 0.7:
-        extract_event(description)
+        event = extract_event(description)
+        result = generate_confirmation(json.dumps(event))
+
+        print(result)
     else:
         print("Not Event")
 
